6870db70111257750c7c8192dc03ff05534fe24a
5MatheusFraga7 <fragamatheus823@gmail.com>
2021-02-25 13:58:29 -0300

    in progress

--- src/controllers/Home.js
+++ src/controllers/Home.js
@@ -1,10 +1,27 @@
 const connection = require('../database/connection');
 
-module.exports = async function (_, response) {
-    const donations = await connection('donations')
+module.exports = async function (request, response) {
+    const { q } = request.query;
+
+    if (!q) {
+        const donations = await connection('donations')
         .orderBy('responsible', 'asc')
         .orderBy('sector', 'asc')
         .select('*');
     
-    return response.render('home', { donations });
+        return response.render('home', { donations });
+    } 
+    else {
+
+        const donations = await connection('donations')
+        .where('responsible', 'like', `%${q}%`)
+        .orWhere('sector', 'like', `%${q}%`)
+        .orderBy('responsible', 'asc')
+        .orderBy('sector', 'asc')
+        .select('*');
+
+        return response.render('home', { search: q, donations })
+    }
+
+
 }
